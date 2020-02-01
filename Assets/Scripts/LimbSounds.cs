654c545e34653e006358a56ab5b7a398c332abf7
NumT <jongaming@me.com>
2020-02-01 21:33:11 +0000

    Added sounds...

---
+++ Assets/Scripts/LimbSounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbSounds : MonoBehaviour
+{
+    public AudioClip[] thudClips;
+    public Vector2 impulseRange;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.impulse.magnitude);
+        float volume = Mathf.InverseLerp(impulseRange.x, impulseRange.y, collision.impulse.magnitude);
+        volume = Mathf.Clamp(volume, 0.5f, 1f);
+        audioSource.volume = volume;
+        PlayClipOneShot(thudClips, false);
+    }
+
+    void PlayClipOneShot(AudioClip[] clips, bool pitchAdjust)
+    {
+        int rand = Random.Range(0, clips.Length);
+        if (pitchAdjust)
+            audioSource.pitch = Random.Range(-0.90f, 1.1f);
+        else
+            audioSource.pitch = 1;
+        audioSource.PlayOneShot(clips[rand]);
+    }
+}
---
+++ Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSounds : MonoBehaviour
+{
+    Animator anim;
+
+
+    AudioSource audioSource;
+    public AudioClip[] throwClips;
+    public AudioClip[] footstepClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnFireLimb(InputValue value)
+    {
+        PlayClipOneShot(throwClips, false);
+    }
+
+    void Footstep()
+    {
+        PlayClipOneShot(footstepClips, false);
+    }
+
+    void PlayClipOneShot(AudioClip[] clips, bool pitchAdjust)
+    {
+        int rand = Random.Range(0, clips.Length);
+        if(pitchAdjust)
+            audioSource.pitch = Random.Range(-0.90f, 1.1f);
+        else
+            audioSource.pitch = 1;
+        audioSource.PlayOneShot(clips[rand]);
+    }
+}
