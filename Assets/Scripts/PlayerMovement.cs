086cb65b1f72761d9f2c790582695e1584238de2
joel <joelxxzz@gmail.com>
2020-02-01 18:06:24 +0000

    Added projectiles, and changed playerinput to be based on rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,55 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody m_rigidbody;
+    private Vector3 m_movement;
+    private bool m_isMoving;
+    private bool m_isGameStart;
+    private float m_moveTimer;
 
-    
     [Header("Input Movement")]
     public KeyCode Forward;
-    public KeyCode Back;
     public KeyCode Left;
     public KeyCode Right;
     public float Speed;
     public float MaxSpeed;
-    [Header("Projectile")]
-    public KeyCode Fire;
-    public float ProjectileSpeed;
-    [Tooltip("Angle in degrees")]
-    public float ProjectileAngle;
-    public GameObject ProjectilePrefab;
-    public Transform ProjectileTransform;
+    
     // Start is called before the first frame update
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_isGameStart = false;
+        PlayerManager.Instance.OnGameStart += OnGameStart;
+        m_moveTimer = 0.0f;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(m_rigidbody.velocity.magnitude > MaxSpeed) return;
+        if (!m_isGameStart) return;
 
-        if(Input.GetKey(Forward))
-        {
-            m_rigidbody.AddForce(Vector3.forward * Speed);
-        }
+        Vector3 preDir = m_rigidbody.velocity.normalized;
 
-        if(Input.GetKey(Back))
-        {
-            m_rigidbody.AddForce(-Vector3.forward * Speed);
-        }
+        Vector3 dir = new Vector3(m_movement.x, 0, m_movement.y);
 
-        if(Input.GetKey(Left))
-        {
-            m_rigidbody.AddForce(-Vector3.right * Speed);
-        }
+        m_rigidbody.velocity =  dir * Speed * Time.deltaTime;
 
-         if(Input.GetKey(Right))
-        {
-            m_rigidbody.AddForce(Vector3.right * Speed);
-        }
+        //if (m_rigidbody.velocity.magnitude < 0.1f)
+        //    m_moveTimer = 0.0f;
+        //else
+        //    m_moveTimer += Time.deltaTime;
+
+        //if (Vector3.Dot(preDir,dir.normalized) < 0.99f ) m_moveTimer = 0.0f;
+        if (m_rigidbody.velocity.magnitude > 0.1f)
+            m_rigidbody.MoveRotation(Quaternion.LookRotation(m_rigidbody.velocity.normalized));
+        //if (m_rigidbody.velocity.magnitude > 0.1f)
+        //    m_rigidbody.MoveRotation(Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(m_rigidbody.velocity.normalized), m_moveTimer));
+
+        //if(m_moveTimer > 1.0f) m_moveTimer = 0.0f;
+
+        print(m_moveTimer);
+    }
+
+    void OnMoveX(InputValue value)
+    {
+        m_movement.x = value.Get<float>();
+    }
+
+    void OnMoveY(InputValue value)
+    {
+        m_movement.y = value.Get<float>();
+    }
+
+    void OnGameStart()
+    {
+        m_isGameStart = true;
     }
 }
---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerProjectile : Projectile
+{
+    //Private projectile member variables
+    private float m_projectileCooldown;
+    private bool m_canFire;
+
+    [Header("Player Projectile")]
+    public GameObject Projectile;
+    public Transform ProjectileSpawnTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_projectileCooldown = 2.0f;
+        m_canFire = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_projectileCooldown -= Time.deltaTime;
+
+
+        if(m_canFire) m_projectileCooldown = 2.0f;
+
+
+        if (m_projectileCooldown < 0.0f) m_canFire = true;
+
+    }
+
+    IEnumerator SpawnProjectile()
+    {
+        GameObject o = Instantiate(Projectile);
+
+        Vector3 location = o.transform.position;
+
+        location = ProjectileSpawnTransform.position;
+
+        o.transform.position = location;
+
+        Rigidbody projectileRigidbody = o.GetComponent<Rigidbody>();
+
+        projectileRigidbody.AddForce((ProjectileSpawnTransform.forward) * ProjectileSpeed);
+
+        yield return null;
+    }
+
+    void OnFireLimb(InputValue value)
+    {
+        m_canFire = false;
+        StartCoroutine(SpawnProjectile());
+    }
+}
