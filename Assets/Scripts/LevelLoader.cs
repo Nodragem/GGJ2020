bcb2af5a68164247f088554b8aa7626ef88d0af9
NumT <jongaming@me.com>
2020-02-02 13:35:11 +0000

    Cutscenes completed

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadSceneAsync("Level", LoadSceneMode.Single);
+    }
+}
