a0025d3ea05c0e83abb274e337e903f82074dc5c
joel <joelxxzz@gmail.com>
2020-02-02 16:22:48 +0000

    Added swithes and switchmanager

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour
+{
+    bool m_isDone;
+
+    public GameObject[] LegNArm;
+    public GameObject ActivationEffect;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_isDone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Limb limb = other.gameObject.GetComponentInParent<Limb>();
+
+        if (limb)
+        {
+            if (m_isDone) return;
+            
+
+            if (limb is Arm arm)
+            {
+                //if (arm.ID == LegNArm[1].GetComponent<Arm>().ID)
+                //{
+                    LegNArm[1].SetActive(true);
+                    //ActivationEffect.SetActive(true);
+                    UpdateManager();
+                    Destroy(limb.gameObject.transform.parent.gameObject);
+                //}
+                //else
+                //{
+                //    Rigidbody rb = other.gameObject.GetComponentInChildren<Rigidbody>();
+
+                //    rb.AddRelativeForce(-rb.velocity / 3);
+                //}
+
+            }
+
+            if (limb is Leg leg)
+            {
+                //if (leg.ID == LegNArm[0].GetComponent<Arm>().ID)
+                //{
+                    LegNArm[0].SetActive(true);
+                    //ActivationEffect.SetActive(true);
+                    UpdateManager();
+                    Destroy(limb.gameObject.transform.parent.gameObject);
+                //}
+                //else
+                //{
+                //    Rigidbody rb = other.gameObject.GetComponentInChildren<Rigidbody>();
+
+                //    rb.AddRelativeForce(-rb.velocity / 3);
+                //}
+
+            }
+
+
+
+        }
+        else if (other.gameObject.GetComponentInChildren<Rigidbody>())
+        {
+            Rigidbody rb = other.gameObject.GetComponentInChildren<Rigidbody>();
+
+            rb.AddRelativeForce(-rb.velocity / 3);
+        }
+
+
+    }
+
+    void UpdateManager()
+    {
+        m_isDone = true;
+        SwitchManager.Instance.UpdateSwitchCounter();
+    }
+}
+
+    
---
+++ Assets/Scripts/SwitchManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchManager : MonoBehaviour
+{
+    public static SwitchManager Instance;
+
+    public Switch[] Switches;
+
+    private int m_switchCounter;
+
+    private int m_switchSize;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_switchCounter = 0;
+        m_switchSize = Switches.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_switchCounter == m_switchSize)
+        {
+            SceneManager.LoadSceneAsync("Cutscene_End");
+        }
+    }
+
+    public void UpdateSwitchCounter()
+    {
+        m_switchCounter++;
+    }
+}
