2d63c7f65d578d53513e2bc6038cf6b16abb7c81
NumT <jongaming@me.com>
2020-02-02 13:45:36 +0000

    Added cutscenes

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,9 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public string level;
     void OnEnable()
     {
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadSceneAsync("Level", LoadSceneMode.Single);
+        SceneManager.LoadSceneAsync(level, LoadSceneMode.Single);
     }
 }
