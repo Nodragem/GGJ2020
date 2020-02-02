0c324c79cb8c55697cc42fbe87dced63273f6ae7
NumT <jongaming@me.com>
2020-02-02 14:49:52 +0000

    Added titlescreen scene

---
+++ Assets/Scripts/TitleLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleLoader : MonoBehaviour
+{
+    public string level;
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+            SceneManager.LoadSceneAsync(level, LoadSceneMode.Single); 
+        }
+    }
+}
